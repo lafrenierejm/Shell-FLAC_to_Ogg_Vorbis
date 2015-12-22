ceedbe40698170f08b70a44309fae46277739ebb
Joseph LaFreniere <lafrenierejm@gmail.com>
2015-12-22 18:52:36 -0500

    Asks for input if destination already exists; tested in bash

--- Flac_to_Ogg_Vorbis.sh
+++ Flac_to_Ogg_Vorbis.sh
@@ -1,33 +1,28 @@
 # Original script provided by will177 on LinuxQuestions.org
 # www.linuxquestions.org/questions/linux-general-1/bash-script-to-convert-flac-album-to-ogg-vorbis-preserving-tags-772235/
 
-if [ $# -lt 1 ]; then
-	echo " "
-	echo "Please supply artist/albumname as argument, e.g. abba/gold"
+if [ $# -lt 2 ]; then
+	echo "Please supply the source of the FLAC files (e.g. ~/Music/FLAC/Artist_Name/Album/) followed by the desired destination (e.g. ~/Music/ogg/Artist_Name/Album/)."
 else
-	SRC_DIR=/music/flac/$1
+	SRC_DIR=$1	# First argument is the source directory
+	DST_DIR=$2	# Second argument is the destination directory
 
 	if [ -d "$SRC_DIR" ]; then
-		echo " "
-		echo "Will Ogg encode the album $1 "
-		echo "from "
-		echo "$SRC_DIR"
-		echo "to "
-		DEST_DIR=/music/ogg/q6/$1
-		echo $DEST_DIR
-		echo " "
-		if [ -d "$DEST_DIR" ]; then
-			echo "Destination already exists, will do nothing."
-		else
-			echo "Ogg encoding album $1 please wait..."
-			mkdir -p $DEST_DIR
-			cd $SRC_DIR
-			for i in *.flac; do oggenc -q6 -o $DEST_DIR/${i%%flac}ogg $i; done
+		if [ -d "$DST_DIR" ]; then	# Prompt if DST_DIR already exists
+			echo "The directory $DST_DIR already exists. Write to it anyway? (Y/N)"
+			read -n 1 writeExisting
+			if [ $writeExisting != "Y" ] && [ $writeExisting != "y" ]; then
+				echo ""
+				exit 1	# If user input something other than 'Y' or 'y', exit script
+			fi
 		fi
+		echo ""	# Print empty line
+		echo "Will Ogg encode the files in ${SRC_DIR} to ${DST_DIR}\n"
+		mkdir -p "$DST_DIR"	# Make the destination directory
+		cd "$SRC_DIR"		# Loop through the source directory
+		for i in *.flac; do oggenc -q6 -o "${DST_DIR}${i%%flac}ogg" "$i"; done
 	else
-		echo "Source directory $SRC_DIR does not exist, doing nothing."
+		echo "Source directory "$SRC_DIR" does not exist"
 	fi
 fi
-
-echo "Finished."
 
